2dab0eb0de584a425795a25a05adfb0fe94ce874
Nicolas Simon <nicolas.simon@generationrobots.com>
2019-06-12 14:01:24 +0200

    av006

--- SFND_Lidar_Obstacle_Detection/src/quiz/cluster/kdtree.h
+++ SFND_Lidar_Obstacle_Detection/src/quiz/cluster/kdtree.h
@@ -28,8 +28,43 @@ struct KdTree
 	void insert(std::vector<float> point, int id)
 	{
 		// TODO: Fill in this function to insert a new point into the tree
-		// the function should create a new node and place correctly with in the root 
+		// the function should create a new node and place correctly with in the root
+		int n = 0;
+		insertHelper(*root, point, id, n);
 
+	}
+
+	void insertHelper(Node **node, std::vector<float> point, int id, int n)
+	{
+		if (*node == NULL)
+		{
+			*node = new Node(point, id);
+		}
+		else if (n == 0)
+		{
+			n = 1;
+			if (point[0] < (*node)->point[0])
+			{
+				insertHelper(&(*node)->left, point, id, n);
+			}
+			else
+			{
+				insertHelper(&(*node)->right, point, id, n);
+			}
+		}
+		else
+		{
+			n = 0;
+			if (point[1] < (*node)->point[1])
+			{
+				insertHelper(&(*node)->left, point, id, n);
+			}
+			else
+			{
+				insertHelper(&(*node)->right, point, id, n);
+			}
+		}
+		
 	}
 
 	// return a list of point ids in the tree that are within distance of target
