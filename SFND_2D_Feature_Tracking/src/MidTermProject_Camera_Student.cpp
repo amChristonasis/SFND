bb5296bc8f4c9408746ca3dc8ccf7eb5c566f59a
Nicolas Simon <nicolas.simon@generationrobots.com>
2019-07-08 13:58:20 +0200

    av018

--- SFND_2D_Feature_Tracking/src/MidTermProject_Camera_Student.cpp
+++ SFND_2D_Feature_Tracking/src/MidTermProject_Camera_Student.cpp
@@ -74,7 +74,7 @@ int main(int argc, const char *argv[])
 
         // extract 2D keypoints from current image
         vector<cv::KeyPoint> keypoints; // create empty feature list for current image
-        string detectorType = "SHITOMASI";
+        string detectorType = "SHITOMASI"; //SHITOMASI, HARRIS, FAST, BRISK, ORB, AKAZE, and SIFT
 
         //// STUDENT ASSIGNMENT
         //// TASK MP.2 -> add the following keypoint detectors in file matching2D.cpp and enable string-based selection based on detectorType
@@ -84,9 +84,13 @@ int main(int argc, const char *argv[])
         {
             detKeypointsShiTomasi(keypoints, imgGray, false);
         }
-        else
+        else if (detectorType.compare("HARRIS") == 0)
         {
-            //...
+            detKeypointsHarris(keypoints, imgGray, false)
+        }
+        else 
+        {
+            detKeypointsModern(keypoints, imgGray, detectorType, false);
         }
         //// EOF STUDENT ASSIGNMENT
 
