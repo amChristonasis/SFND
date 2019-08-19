da95e3b76ac77b3214abfb8d94ea0088e4108449
Nicolas Simon <nicolas.simon@generationrobots.com>
2019-08-19 13:47:59 +0200

    av028

--- SFND_3D_Object_Tracking/src/camFusion_Student.cpp
+++ SFND_3D_Object_Tracking/src/camFusion_Student.cpp
@@ -133,7 +133,23 @@ void show3DObjects(std::vector<BoundingBox> &boundingBoxes, cv::Size worldSize, 
 // associate a given bounding box with the keypoints it contains
 void clusterKptMatchesWithROI(BoundingBox &boundingBox, std::vector<cv::KeyPoint> &kptsPrev, std::vector<cv::KeyPoint> &kptsCurr, std::vector<cv::DMatch> &kptMatches)
 {
-    // ...
+    for (auto it = kptMatches.begin(); it != kptMatches.end(); ++it)
+    {
+        if (kptsPrev[it->trainIdx].pt.x > boundingBox.roi.x && kptsPrev[it->trainIdx].pt.x < boundingBox.roi.x + boundingBox.roi.height)
+        {
+            if (kptsPrev[it->trainIdx].pt.y > boundingBox.roi.y && kptsPrev[it->trainIdx].pt.y < boundingBox.roi.y + boundingBox.roi.width)
+            {
+                if (kptsCurr[it->trainIdx].pt.x > boundingBox.roi.x && kptsCurr[it->trainIdx].pt.x < boundingBox.roi.x + boundingBox.roi.height)
+                {
+                    if (kptsCurr[it->trainIdx].pt.y > boundingBox.roi.y && kptsCurr[it->trainIdx].pt.y < boundingBox.roi.y + boundingBox.roi.width)
+                    {
+                        boundingBox.keypoints.push_back(kptsCurr[it->trainIdx]);
+                        boundingBox.kptMatches.push_back(*it);
+                    }
+                }
+            }
+        }
+    }
 }
 
 
@@ -141,7 +157,50 @@ void clusterKptMatchesWithROI(BoundingBox &boundingBox, std::vector<cv::KeyPoint
 void computeTTCCamera(std::vector<cv::KeyPoint> &kptsPrev, std::vector<cv::KeyPoint> &kptsCurr, 
                       std::vector<cv::DMatch> kptMatches, double frameRate, double &TTC, cv::Mat *visImg)
 { 
-    // ...
+    // compute distance ratios between all matched keypoints
+    vector<double> distRatios; // stores the distance ratios for all keypoints between curr. and prev. frame
+    for (auto it1 = kptMatches.begin(); it1 != kptMatches.end() - 1; ++it1)
+    { // outer kpt. loop
+
+        // get current keypoint and its matched partner in the prev. frame
+        cv::KeyPoint kpOuterCurr = kptsCurr.at(it1->trainIdx);
+        cv::KeyPoint kpOuterPrev = kptsPrev.at(it1->queryIdx);
+
+        for (auto it2 = kptMatches.begin() + 1; it2 != kptMatches.end(); ++it2)
+        { // inner kpt.-loop
+
+            double minDist = 100.0; // min. required distance
+
+            // get next keypoint and its matched partner in the prev. frame
+            cv::KeyPoint kpInnerCurr = kptsCurr.at(it2->trainIdx);
+            cv::KeyPoint kpInnerPrev = kptsPrev.at(it2->queryIdx);
+
+            // compute distances and distance ratios
+            double distCurr = cv::norm(kpOuterCurr.pt - kpInnerCurr.pt);
+            double distPrev = cv::norm(kpOuterPrev.pt - kpInnerPrev.pt);
+
+            if (distPrev > std::numeric_limits<double>::epsilon() && distCurr >= minDist)
+            { // avoid division by zero
+
+                double distRatio = distCurr / distPrev;
+                distRatios.push_back(distRatio);
+            }
+        } // eof inner loop over all matched kpts
+    }     // eof outer loop over all matched kpts
+
+    // only continue if list of distance ratios is not empty
+    if (distRatios.size() == 0)
+    {
+        TTC = NAN;
+        return;
+    }
+
+    std::sort(distRatios.begin(), distRatios.end());
+    long medIndex = floor(distRatios.size() / 2.0);
+    double medDistRatio = distRatios.size() % 2 == 0 ? (distRatios[medIndex - 1] + distRatios[medIndex]) / 2.0 : distRatios[medIndex]; // compute median dist. ratio to remove outlier influence
+
+    double dT = 1 / frameRate;
+    TTC = -dT / (1 - medDistRatio);
 }
 
 
